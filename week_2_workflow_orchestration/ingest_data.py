83063d667988752398a4871087ffcfa10767ea02
Lily Grier <lily.grier@justiceinnovationlab.org>
2023-02-01 00:04:38 +0000

    REFACTOR using prefect

--- week_2_workflow_orchestration/ingest_data.py
+++ week_2_workflow_orchestration/ingest_data.py
@@ -8,18 +8,12 @@ from time import time
 
 import pandas as pd
 from sqlalchemy import create_engine
+from prefect import flow, task
+from prefect.tasks import task_input_hash
+from datetime import timedelta
 
-
-# def main(params):
-#     user = params.user
-#     password = params.password
-#     host = params.host 
-#     port = params.port 
-#     db = params.db
-#     table_name = params.table_name
-#     url = params.url
-    
-def ingest_data(user, password, host, port, db, table_name, url):
+@task(log_prints=True, retries=3, cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
+def extract_data(url):
     # the backup files are gzipped, and it's important to keep the correct extension
     # for pandas to be able to open the file
     if url.endswith('.csv.gz'):
@@ -29,58 +23,44 @@ def ingest_data(user, password, host, port, db, table_name, url):
 
     os.system(f"wget {url} -O {csv_name}")
 
-    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
-
     df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
 
     df = next(df_iter)
 
     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    return df
+
+@task(log_prints=True)
+def transform_data(df):
+    print(f"pre: missing passenger count: {df['passenger_count'].isin([0]).sum()}")
+    df = df[df['passenger_count'] != 0]
+    print(f"post: missing passenger count: {df['passenger_count'].isin([0]).sum()}")
+    return df
+
+@task(log_prints=True, retries=3)
+def ingest_data(user, password, host, port, db, table_name, df):
+    
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
     df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
-
     df.to_sql(name=table_name, con=engine, if_exists='append')
 
 
-    while True: 
-
-        try:
-            t_start = time()
-            
-            df = next(df_iter)
-
-            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-
-            df.to_sql(name=table_name, con=engine, if_exists='append')
-
-            t_end = time()
-
-            print('inserted another chunk, took %.3f second' % (t_end - t_start))
-
-        except StopIteration:
-            print("Finished ingesting data into the postgres database")
-            break
-
-if __name__ == '__main__':
-    # parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
-
-    # parser.add_argument('--user', required=True, help='user name for postgres')
-    # parser.add_argument('--password', required=True, help='password for postgres')
-    # parser.add_argument('--host', required=True, help='host for postgres')
-    # parser.add_argument('--port', required=True, help='port for postgres')
-    # parser.add_argument('--db', required=True, help='database name for postgres')
-    # parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
-    # parser.add_argument('--url', required=True, help='url of the csv file')
-
-    # args = parser.parse_args()
+@flow(name="Ingest Flow")
+def main_flow(table_name: str):
     user = 'root'
     password = 'root'
     host = 'localhost'
     port = '5432'
     db = 'ny_taxi'
-    table_name = 'yellow_taxi_trips'
+    table_name = table_name
     url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"    
 
-    ingest_data(user, password, host, port, db, table_name, url)
+    raw_data = extract_data(url)
+    data = transform_data(raw_data)
+    ingest_data(user, password, host, port, db, table_name, data)
+
+
+if __name__ == '__main__':
+    main_flow("yellow_taxi_trips")
