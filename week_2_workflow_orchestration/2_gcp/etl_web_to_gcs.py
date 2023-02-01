84e2f10ffc08335011cfc879dea23de95ff2bbba
lilygrierjil <lily.grier@justiceinnovationlab.org>
2023-02-01 20:24:03 +0000

    ADD cleaning function

--- week_2_workflow_orchestration/2_gcp/etl_web_to_gcs.py
+++ week_2_workflow_orchestration/2_gcp/etl_web_to_gcs.py
@@ -2,13 +2,24 @@ from pathlib import Path
 import pandas as pd
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
+from random import randint
 
 @task(retries=3)
 def fetch(dataset_url: str) -> pd.DataFrame:
     """Read taxi data from web into pandas DataFrame"""
-
+    # if randint(0, 1) > 0:
+    #     raise Exception
     df = pd.read_csv(dataset_url)
     return df
+
+@task(log_prints=True)
+def clean(df: pd.DataFrame) -> pd.DataFrame:
+    """Fix dtype issues"""
+    df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
+    df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
+    print(df.head(2))
+    print(f"columns: {df.dtypes}")
+    print(f"rows: {len(df)}")
 
 @flow()
 def etl_web_to_gcs() -> None:
@@ -20,6 +31,7 @@ def etl_web_to_gcs() -> None:
     dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"    
     # print(dataset_url)
     df = fetch(dataset_url)
+    df_clean = clean(df)
 
 if __name__=='__main__':
     etl_web_to_gcs()
