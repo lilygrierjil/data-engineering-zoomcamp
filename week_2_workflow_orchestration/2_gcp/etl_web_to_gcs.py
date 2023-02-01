67c81d066e2508af6d135f50e62161e8e58ec33c
lilygrierjil <lily.grier@justiceinnovationlab.org>
2023-02-01 21:33:11 +0000

    ADD bigquery code

--- week_2_workflow_orchestration/2_gcp/etl_web_to_gcs.py
+++ week_2_workflow_orchestration/2_gcp/etl_web_to_gcs.py
@@ -20,6 +20,25 @@ def clean(df: pd.DataFrame) -> pd.DataFrame:
     print(df.head(2))
     print(f"columns: {df.dtypes}")
     print(f"rows: {len(df)}")
+    return df
+
+@task()
+def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
+    """Write DataFrame out as parquet file"""
+    path = Path(f"data/{color}/{dataset_file}.parquet")
+    print(path)
+    df.to_parquet(path, compression="gzip")
+    return path
+
+@task()
+def write_gcs(path: Path) -> None:
+    """Upload local parquet file to GCS"""
+    gcs_block = GcsBucket.load("zoom-gcs")
+    gcs_block.upload_from_path(
+        from_path=path,
+        to_path=path
+    )
+    return
 
 @flow()
 def etl_web_to_gcs() -> None:
@@ -32,6 +51,8 @@ def etl_web_to_gcs() -> None:
     # print(dataset_url)
     df = fetch(dataset_url)
     df_clean = clean(df)
+    path = write_local(df_clean, color, dataset_file)
+    write_gcs(path)
 
 if __name__=='__main__':
     etl_web_to_gcs()
