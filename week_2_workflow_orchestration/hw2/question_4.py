2f5f94f30725ddf1ba2249e03256c9cc6abf71bd
lilygrierjil <lily.grier@justiceinnovationlab.org>
2023-02-06 04:02:15 +0000

    ADD code for q4

---
+++ week_2_workflow_orchestration/hw2/question_4.py
@@ -0,0 +1,14 @@
+from prefect.filesystems import GitHub
+from prefect.deployments import Deployment
+from question_4_a import etl_web_to_gcs
+
+github_block = GitHub.load("zoomcamp-github")
+
+github_dep = Deployment.build_from_flow(
+    flow=etl_web_to_gcs,
+    name='github-flow',
+    entrypoint='week_2_workflow_orchestration/hw2/question_4_a.py:etl_web_to_gcs'
+)
+
+if __name__=="__main__":
+    github_dep.apply()
