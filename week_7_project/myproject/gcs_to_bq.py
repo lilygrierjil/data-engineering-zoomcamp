b57cfdef1f0ac5c5448ceeaf1d53160769acfd59
lilygrierjil <lily.grier@justiceinnovationlab.org>
2023-03-21 23:09:34 +0000

    ADD bigquery dataset creation

--- week_7_project/myproject/gcs_to_bq.py
+++ week_7_project/myproject/gcs_to_bq.py
@@ -1,16 +1,16 @@
-from prefect_gcp.bigquery import BigQueryWarehouse
+from prefect_gcp.bigquery import BigQueryWarehouse, bigquery_query
 from prefect_gcp import GcpCredentials
 from prefect import flow, task
 
 @task()
 def create_bq_dataset():
     gcp_credentials_block = GcpCredentials.load("zoom-gcp-creds")
-    client = gcp_credentials_block.get_bigquery_client()
+    client = gcp_credentials_block.get_bigquery_client(project='buoyant-song-375701')
     client.create_dataset("final_project", exists_ok=True)
 
 @task()
 def create_external_table():
-    warehouse = BigQueryWarehouse.load("final-project-bq")
+    warehouse = BigQueryWarehouse.load("final-project-bq", project='buoyant-song-375701')
     create_operation = '''
     CREATE OR REPLACE EXTERNAL TABLE `final_project.external_memphis_police_data`
     OPTIONS (
@@ -18,6 +18,7 @@ def create_external_table():
     uris = ['gs://prefect-de-zoomcamp-lily/data/memphis_police_data.parquet']
     );
     '''
+    gcp_credentials_block = GcpCredentials.load("zoom-gcp-creds")
     warehouse.execute(create_operation)
 
 @task()
@@ -35,6 +36,7 @@ def create_partitioned_clustered_table():
 def etl_gcs_to_bq():
     gcp_credentials_block = GcpCredentials.load("zoom-gcp-creds")
     google_auth_credentials = gcp_credentials_block.get_credentials_from_service_account()
+    print('credentials!')
     print(google_auth_credentials)
     create_bq_dataset()
     create_external_table()
