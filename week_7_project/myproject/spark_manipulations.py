05e9eb0bdf2c1f8c3806edebe9dd304b01944981
lilygrierjil <lily.grier@justiceinnovationlab.org>
2023-03-28 16:29:06 +0000

    ADD spark py file

--- week_7_project/myproject/spark_manipulations.py
+++ week_7_project/myproject/spark_manipulations.py
@@ -1,6 +1,7 @@
 import pyspark
 from pyspark.sql import SparkSession
 from pyspark.sql import types
+from pyspark.sql.functions import to_date
 
 
 
@@ -18,3 +19,33 @@ mpd_schema = types.StructType([
     types.StructField("offense_date_datetime", types.TimestampType(), True),
 ])
 
+# start a spark session
+spark = SparkSession.builder \
+    .appName('memphis-police-data') \
+    .getOrCreate()
+
+# read from big query
+bucket = "[bucket]"
+spark.conf.set('temporaryGcsBucket', bucket)
+
+df = spark.read.format('bigquery') \
+  .schema(mpd_schema) \
+  .option('table', 'mephis_police_data_all.memphis_police_data_partitioned_clustered') \
+  .load()
+
+df = df.withColumn('offense_day', to_date('offense_date_datetime'))
+
+df.registerTempTable('memphis')
+
+daily_grouped = spark.sql('''
+SELECT agency_crimetype_id AS crime_type,
+offense_day,
+FIRST(category) AS crime_type_category,
+COUNT(crime_id) AS daily_crime_type_count
+FROM memphis
+GROUP BY 1, 2
+''')
+                       
+daily_grouped.write.format('bigquery') \
+  .option('table', 'memphis_police_data_all.daily_crime_type_counts') \
+  .save()
