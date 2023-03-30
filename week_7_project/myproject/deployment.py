922b9d896f52aa312a828a135d31c6fa7984b651
lilygrierjil <lily.grier@justiceinnovationlab.org>
2023-03-30 21:03:25 +0000

    ADD deployment code

--- week_7_project/myproject/deployment.py
+++ week_7_project/myproject/deployment.py
@@ -1,10 +1,22 @@
-from my_project.flows import my_flow
+from flows.full_etl import main_flow
 from prefect.deployments import Deployment
+from prefect.orion.schemas.schedules import IntervalSchedule
+
 
 deployment = Deployment.build_from_flow(
-    flow=my_flow,
-    name="example-deployment", 
+    flow=main_flow,
+    name="one-time-deployment", 
     version=1, 
-    work_queue_name="demo",
+    work_queue_name="default",
 )
-deployment.apply()
+
+daily_deployment = Deployment.build_from_flow(
+    flow=main_flow,
+    name="daily-deployment", 
+    version=1, 
+    work_queue_name="default",
+    schedule=(IntervalSchedule(interval=86400))
+)
+if __name__=='__main__':
+    deployment.apply()
+    daily_deployment.apply()
